b70bc45030f815de399f6b7e2dec255120d4d4b2
ZTCooper <ztcooper@outlook.com>
2017-10-25 10:54:29 +0800

    棋盘拼接问题ok了 XD

--- chapter4_归纳递归归简/棋盘拼接问题（递归）.py
+++ chapter4_归纳递归归简/棋盘拼接问题（递归）.py
@@ -1,6 +1,9 @@
 #board棋盘矩阵，size边长，tr方格左上行数，tc方格左上列数，dr缺失格子行数，dc缺失格子列数
 def chessboard(board, size, tr, tc, dr, dc):
-	lable = 0
+	if size <= 1:
+		return
+
+	global lable
 	lable += 1
 	size //= 2
 	#缺失格子位于第一象限
@@ -25,10 +28,12 @@ def chessboard(board, size, tr, tc, dr, dc):
 	if dr >= tr + size and dc >= tc + size:
 		chessboard(board, size, tr + size, tc + size, dr, dc)
 	else:
-		borad[tr + size][tc + size] = lable
+		board[tr + size][tc + size] = lable
 		chessboard(board, size, tr + size, tc + size, tr + size, tc + size)
 
-size = 8
-board = [[0 for i in range(size)] for j in range(size)]
-chessboard(board, size, 0, 0, 0, 7)	#右上角缺失的8*8方格
-print(board)
+
+lable = 0
+board = [[0 for i in range(8)] for j in range(8)]
+chessboard(board, 8, 0, 0, 0, 7)	#右上角缺失的8*8方格
+for row in board:
+	print(row)
