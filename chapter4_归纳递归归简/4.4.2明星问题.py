b81973323276e68fb375554161eb9f0f070b6c5e
ZTCooper <ztcooper@outlook.com>
2017-11-10 21:46:01 +0800

    明星问题解决方案

--- chapter4_归纳递归归简/4.4.2明星问题.py
+++ chapter4_归纳递归归简/4.4.2明星问题.py
@@ -12,3 +12,28 @@ def naive_celeb(G):
 		else:
 			return u			#no break, is Celebrity
 	return None					#couldn't find anyone
+
+#明星问题的解决方案
+#要么此人认识其他人，要么谁也不认识此人
+def celeb(G):
+	n = len(G)
+	u,v = 0,1					#the first two
+	for c in range(2, n + 1):	#others to check
+		if G[u][v]:				#u knows v
+			u = c  				#replace u with c
+		else:					#u doesn't know v
+			v = c 				#replace v
+	if u == n:					#u was replaced last
+		c = v					#use v
+	else:						#otherwise
+		c = u 					#u is a candidate
+	for v in range(n):			#for everyone else
+		if c == v:				#same person
+			continue			#skip
+		if [c][v]:				#candidate knows others
+			break				
+		if not G[v][c]:			#other doesn't know csndidate
+			break
+	else:						#no breaks
+		return c 				#c is celebrity
+	return None					#couldn't find anyone
