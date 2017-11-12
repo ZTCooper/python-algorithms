6416ca24795a6f3448211e671ca9d0185c7aebcf
ZTCooper <ztcooper@outlook.com>
2017-11-12 14:55:24 +0800

    DAG拓扑排序（删除无入度点）

--- chapter4_归纳递归归简/4.4.3拓扑排序.py
+++ chapter4_归纳递归归简/4.4.3拓扑排序.py
@@ -23,5 +23,25 @@ def naive_topsort(G, S = None):
 	seq.insert(min_i, v)
 	return seq
 
+#有向无环路图DAG(Directed Acyclic Graph)的拓扑排序
+#删除无入度的点
+def topsort(G):
+	count = dict((u, 0) for u in G)	
+	#{'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0}
+	for u in G:
+		for v in G[u]:
+			count[v] += 1		#in_degree
+	Q = [u for u in G if count[u] == 0]		#nodes with no in_degree
+	S = []		#result
+	while Q:
+		u = Q.pop()
+		S.append(u)
+		for v in G[u]:
+			count[v] -= 1		#uncount out_degree
+			if count[v] == 0:	#new node without in_degree
+				Q.append(v)
+	return S
+
 if __name__ == "__main__":
 	print(naive_topsort(G))
+	print(topsort(G))
