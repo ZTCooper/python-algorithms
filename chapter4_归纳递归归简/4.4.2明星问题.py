00461b42dfacf2e07a414da6c784782c82224cdd
ZTCooper <ztcooper@outlook.com>
2017-11-12 11:30:38 +0800

    明星问题构造随机图并指定明星

--- chapter4_归纳递归归简/4.4.2明星问题.py
+++ chapter4_归纳递归归简/4.4.2明星问题.py
@@ -1,3 +1,16 @@
+#构造随机图
+from random import randrange
+n = 1000
+G = [[randrange(2) for i in range(n)] for i in range(n)]
+#randrange(2) -> 0 or 1
+#[[0, ..., 1], [0, ..., 0], ..., [0, ..., 1]]
+
+#指定明星
+c = randrange(n)	#one in 0-999
+for i in range(n):
+	G[i][c] = True		#others know c
+	G[c][i] = False		#c doesn't know others
+
 #朴素版的明星问题方案
 def naive_celeb(G):
 	n = len(G)
@@ -30,10 +43,14 @@ def celeb(G):
 	for v in range(n):			#for everyone else
 		if c == v:				#same person
 			continue			#skip
-		if [c][v]:				#candidate knows others
+		if G[c][v]:				#candidate knows others
 			break				
 		if not G[v][c]:			#other doesn't know csndidate
 			break
 	else:						#no breaks
 		return c 				#c is celebrity
 	return None					#couldn't find anyone
+
+if __name__ == '__main__':
+	print(naive_celeb(G))
+	print(celeb(G))
